52f0d3adbbdc09e5ce40d9c2669bd01d7117c331
Nguyễn Xuân Phú <49805176+nguyenphu04723@users.noreply.github.com>
2019-09-30 12:06:16 +0700

    Update and rename loading.css to loading.js

---
+++ loading.js
@@ -0,0 +1,5 @@
+<script>
+        pageLoad()
+function pageLoad(){var doc=document.documentElement;window.addEventListener('load',(e)=>{doc.classList.remove('loading')})}
+    </script>
+// <html class='loading' dir='ltr' lang='vi' prefix='og: http://ogp.me/ns#' xmlns='http://www.w3.org/1999/xhtml' xmlns:b='http://www.google.com/2005/gml/b' xmlns:data='http://www.google.com/2005/gml/data' xmlns:expr='http://www.google.com/2005/gml/expr'>
